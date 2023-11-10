83e242f1c27b5f8310c269cedd0e563313707205
Phillip Lam <plam89@ucla.edu>
2023-11-09 19:51:05 -0800

    project setup

---
+++ index.js
@@ -0,0 +1,36 @@
+
+window.onload = function() {
+  //canvas setup
+  const canvas = document.getElementById('canvas1')
+  const ctx = canvas.getContext('2d')
+  canvas.width = 500
+  canvas.height = 500
+
+  class InputHandler {
+
+  }
+  class Projectile {
+
+  }
+  class Particle {
+
+  }
+  class Player {
+
+  }
+  class Enemy {
+
+  }
+  class Layer {
+
+  }
+  class Background { //pools Layer classes
+
+  }
+  class UI {
+
+  }
+  class Game {
+    
+  }
+}

ed04920ea8a799333e09e91024ae8de5e425cf79
Phillip Lam <plam89@ucla.edu>
2024-02-28 19:06:41 -0800

    added 3 different types of enemies with different spawn probabilities

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   const canvas = document.getElementById('canvas1')
   const ctx = canvas.getContext('2d')
 
-  canvas.width = 500
+  canvas.width = 800
   canvas.height = 500
 
   class InputHandler {
@@ -69,6 +69,9 @@
       this.maxSpeed = 3
       this.projectiles = []
       this.image = document.getElementById('player')
+      this.isPoweredUp = false
+      this.powerUpTimer = 0
+      this.powerUpLimit = 10000
     
     }
     update() {
@@ -95,11 +98,21 @@
 
       if (this.frameX < this.maxFrame) this.frameX++
       else this.frameX = 0
+
+      //handle power up
+      if (this.isPoweredUp) {
+        if(this.powerUpTimer > this.powerUpLimit) {
+          this.powerUpTimer = 0
+          this.isPoweredUp = false
+          this.frameY = 0 //so we only render the NON-POWERED UP image
+        }
       }
+    }
+      
 
     draw(context) { //better to pass in our ctx as an argument
       if (this.game.debug) {
-        context.lineWidth = 5
+        context.lineWidth = 2
         context.strokeRect(this.x, this.y, this.width, this.height)  
       }   
       context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x,this.y, this.width,this.height)
@@ -120,21 +133,33 @@
       constructor(game) {
         this.game = game
         this.x = this.game.width
-        this.speedX = Math.random() * -1.5 - 0.5
+        this.speedX = Math.random() * - 0.34
         this.markedForDeletion = false
-        this.lives = 5
-        this.score = this.lives
+
+        this.frameX = 0
+        this.frameY = 0
+        this.maxFrame = 37
+
       }
       update() {
-        this.x+= this.speedX
+        this.x+= this.speedX - this.game.speed
         if (this.x + this.width < 0) this.markedForDeletion = true
+        //sprite animation
+
       }
       draw(context) {
-          context.fillStyle = 'red'
-          context.fillRect(this.x ,this.y, this.width, this.height)
-          context.fillStyle = 'black'
-          context.font =  '20px Helvetica'
-          context.fillText(this.lives, this.x + (this.width * .4) , this.y + 25)
+          if (this.game.debug) {
+            context.strokeStyle = 'red'
+            context.strokeRect(this.x ,this.y, this.width, this.height)
+            context.font =  '20px Helvetica'
+            context.fillStyle = 'goldenrod'
+            context.fillText(this.lives, this.x + (this.width * .4) , this.y - 5)
+          }
+          if (this.frameX < this.maxFrame) {
+            this.frameX++
+          }else this.frameX = 0
+          context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width,this.height, this.x,this.y,this.width,this.height)
+
 
       }
   }
@@ -142,13 +167,40 @@
 class Angler1 extends Enemy { //Angler1 is a child of Enemy, all methods that cannot be find on Angler1 will look at Enemy's props/methods
   constructor(game) { //if constructor is not declared here, all new Angler1's will run Enemy's (aka parent's) constructor instead!
     super(game) // calling super here will make sure the Parent Class aka Enemy's constructor will run FIRST, THEN the Angler1 constructor runs
-    this.width = 228 * .2
-    this.height = 169 * .2
+    this.width = 228 
+    this.height = 169 
+    this.lives = 3
+    this.score = this.lives
     this.y = Math.random() * (this.game.height  - this.game.player.height)
-    
+    this.image = document.getElementById('angler1')
+    this.frameY = ~~(Math.random() * 3)
   }
 }
-
+class Angler2 extends Enemy { 
+  constructor(game) { 
+    super(game) 
+    this.width = 213
+    this.height = 165 
+    this.lives = 5
+    this.score = this.lives
+    this.y = Math.random() * (this.game.height  - this.game.player.height)
+    this.image = document.getElementById('angler2')
+    this.frameY = ~~(Math.random() * 2)
+  }
+}
+class LuckyFish extends Enemy {
+  constructor(game) { 
+    super(game) 
+    this.width = 99
+    this.height = 95 
+    this.lives = 3
+    this.score = this.lives * 4
+    this.type = 'lucky'
+    this.y = Math.random() * (this.game.height  - this.game.player.height)
+    this.image = document.getElementById('lucky')
+    this.frameY = ~~(Math.random() * 2)
+  } 
+}
 class Layer { //sets up all 4 layer images 
   constructor(game, img, speedMod) {
     this.game = game
@@ -170,187 +222,211 @@ class Layer { //sets up all 4 layer images
   }
 
   }
-  class Background { //pools the 4 Layer imgs together
-    constructor(game) {
-      this.game = game
-      this.images = [...document.querySelectorAll('.bgLayer')] //must convert to array so we can use map method below
-      this.layers = this.images.map((img) => new Layer(this.game,img, .66))
-      this.lastLayer = new Layer(this.game, document.querySelector('.bgLayer4'), 0.55)
-      // this.image1 = document.querySelector('#layer1')
-      // this.image2 = document.querySelector('#layer2')
-      // this.image3 = document.querySelector('#layer3')
-      // this.image4 = document.querySelector('#layer4')
+class Background { //pools the 4 Layer imgs together
+  constructor(game) {
+    this.game = game
+    this.images = [...document.querySelectorAll('.bgLayer')] //must convert to array so we can use map method below
+    this.layers = this.images.map((img) => new Layer(this.game,img, .66))
+    this.lastLayer = new Layer(this.game, document.querySelector('.bgLayer4'), 0.55)
+    // this.image1 = document.querySelector('#layer1')
+    // this.image2 = document.querySelector('#layer2')
+    // this.image3 = document.querySelector('#layer3')
+    // this.image4 = document.querySelector('#layer4')
 
-      // this.layer1 = new Layer(this.game, this.image1, 2)
-      // this.layer2 = new Layer(this.game, this.image2, 2)
-      // this.layer3 = new Layer(this.game, this.image3, 2)
-      // this.layer4 = new Layer(this.game, this.image4, 2)
+    // this.layer1 = new Layer(this.game, this.image1, 2)
+    // this.layer2 = new Layer(this.game, this.image2, 2)
+    // this.layer3 = new Layer(this.game, this.image3, 2)
+    // this.layer4 = new Layer(this.game, this.image4, 2)
 
-      // this.layers = [this.layer1,this.layer2,this.layer3,this.layer4]
-    }
-    update() {
-      this.layers.forEach(layer => layer.update())
-    }
-    draw(context) {
-      this.layers.forEach(layer => layer.draw(context))
-    }
-    updateLastLayer() { //so the layer 4 appears IN-FRONT of our player but our player  appears IN FRONT of all other layers
-      this.lastLayer.update()
-    }
-    drawLastLayer(ctx) {
-      this.lastLayer.draw(ctx)
-    }
+    // this.layers = [this.layer1,this.layer2,this.layer3,this.layer4]
   }
-  class UI {
-    constructor(game) {
-      this.game = game
-      this.fontSize = 25
-      this.fontFamily = 'Helvetica'
-      this.color = 'white'
-    }
-    draw(context) {
-      context.save()
-      context.fillStyle = this.color
-      context.ShadowOffsetX = 2
-      context.ShadowOffsetY = 2
-      context.shadowColor = 'black'
-      context.font = this.fontSize + 'px ' + this.fontFamily
-
-      //display score
-      context.fillText('Score: ' + this.game.score, 20, 70)
-      //ammo
-
-      for (let i = 0; i < this.game.ammo; i++) {
-        context.fillStyle = 'darkviolet'
-        context.fillRect(20 + 10 * i,10,3,20)
-      }
-      //timer
-      const formattedTime = ~~(this.game.gameTime * 0.001)
-      context.fillText('Timer: ' + formattedTime + 's', 20, 100)
-      // game over msgs 
-      if (this.game.gameOver) {
-        context.textAlign = 'center'
-        let msg1
-        let msg2
-        if (this.game.score >= this.game.winningScore) {
-          msg1 = 'You win!'
-          msg2 = 'Well Done!'
-        } else {
-          msg1 = 'You Lose!'
-          msg2 = 'You Suck, Try Again!'
-        }
-        context.font = '50px ' + this.fontFamily
-        context.fillText(msg1, this.game.width / 2, this.game.height * .5)
-        context.font = '25px ' + this.fontFamily
-        context.fillText(msg2, this.game.width / 2, this.game.height * .5 - 50)
-      }
-      context.restore()
-    }
+  update() {
+    this.layers.forEach(layer => layer.update())
   }
-  class Game {
-    constructor(width,height) { //takes in global canvas width / height
-      this.width = width
-      this.height = height
-      this.backGround = new Background(this)
-      this.player = new Player(this) //a new instance of player will be created EVERY time a game instance is created!
-      // passing 'this' will pass the whole Game object, now Player instances will have access to Game object
-      this.input = new InputHandler(this)
-      this.ui = new UI(this)
-      this.keys = []
-      this.enemies = []
-      this.enemyTimer = 0
-      this.enemyInterval = 666
-      this.ammo = 20
-      this.maxAmmo = 60      
-      this.ammoTimer = 0
-      this.score = 0
-      this.winningScore = 5555
-      this.ammoInterval = 200
-      this.gameOver = false
-      this.gameTime = 0
-      this.timeLimit = 5000 //5s to test
-      this.speed = 1
-      this.debug = false
-    }
-    update(deltaTime) {
-      if (!this.gameOver) this.gameTime+= deltaTime
-      // if (this.gameTime >= this.timeLimit) this.gameOver = true
+  draw(context) {
+    this.layers.forEach(layer => layer.draw(context))
+  }
+  updateLastLayer() { //so the layer 4 appears IN-FRONT of our player but our player  appears IN FRONT of all other layers
+    this.lastLayer.update()
+  }
+  drawLastLayer(ctx) {
+    this.lastLayer.draw(ctx)
+  }
+}
+class UI {
+  constructor(game) {
+    this.game = game
+    this.fontSize = 25
+    this.fontFamily = 'Helvetica'
+    this.color = 'white'
+  }
+  draw(context) {
+    context.save()
+    context.fillStyle = this.color
+    context.ShadowOffsetX = 2
+    context.ShadowOffsetY = 2
+    context.shadowColor = 'black'
+    context.font = this.fontSize + 'px ' + this.fontFamily
 
-      this.backGround.update()
-      this.player.update() 
-      this.backGround.updateLastLayer()
-      //if game object calls update, then player object ALSO calls its update
-      if (this.ammoTimer > this.ammoInterval) {
-           if (this.ammo < this.maxAmmo) this.ammo++
-           this.ammoTimer = 0
+    //display score
+    context.fillText('Score: ' + this.game.score, 20, 70)
+    //ammo
+
+    for (let i = 0; i < this.game.ammo; i++) {
+      context.fillStyle = 'darkviolet'
+      context.fillRect(20 + 10 * i,10,3,20)
+    }
+    //timer
+    const formattedTime = ~~(this.game.gameTime * 0.001)
+    context.fillText('Timer: ' + formattedTime + 's', 20, 100)
+    // game over msgs 
+    if (this.game.gameOver) {
+      context.textAlign = 'center'
+      let msg1
+      let msg2
+      if (this.game.score >= this.game.winningScore) {
+        msg1 = 'You win!'
+        msg2 = 'Well Done!'
       } else {
-          this.ammoTimer+= deltaTime
+        msg1 = 'You Lose!'
+        msg2 = 'You Suck, Try Again!'
       }
-      this.enemies.forEach(enemy => {
-        enemy.update()
-        if (this.checkCollision(this.player, enemy)) {
-            enemy.markedForDeletion = true
-        }
-        this.player.projectiles.forEach(projectile => {
-          if (this.checkCollision(projectile, enemy)) {
-              //marking for deletion will make removing easier
-              projectile.markedForDeletion = true
-              enemy.lives--
-              
+      context.font = '50px ' + this.fontFamily
+      context.fillText(msg1, this.game.width / 2, this.game.height * .5)
+      context.font = '25px ' + this.fontFamily
+      context.fillText(msg2, this.game.width / 2, this.game.height * .5 - 50)
+    }
+    context.restore()
+  }
+}
+class Game {
+  constructor(width,height) { //takes in global canvas width / height
+    this.width = width
+    this.height = height
+    this.backGround = new Background(this)
+    this.player = new Player(this) //a new instance of player will be created EVERY time a game instance is created!
+    // passing 'this' will pass the whole Game object, now Player instances will have access to Game object
+    this.input = new InputHandler(this)
+    this.ui = new UI(this)
+    this.keys = []
+    this.enemies = []
+    this.enemyTimer = 0
+    this.enemyInterval = 666
+    this.ammo = 20
+    this.maxAmmo = 60      
+    this.ammoTimer = 0
+    this.score = 0
+    this.winningScore = 5555
+    this.ammoInterval = 200
+    this.gameOver = false
+    this.gameTime = 0
+    this.timeLimit = 5000 //5s to test
+    this.speed = 1
+    this.debug = false
+  }
+  update(deltaTime) {
+    if (!this.gameOver) this.gameTime+= deltaTime
+    // if (this.gameTime >= this.timeLimit) this.gameOver = true
+
+    this.backGround.update()
+    this.player.update() 
+    this.backGround.updateLastLayer()
+    //if game object calls update, then player object ALSO calls its update
+    if (this.ammoTimer > this.ammoInterval) {
+          if (this.ammo < this.maxAmmo) this.ammo++
+          this.ammoTimer = 0
+    } else {
+        this.ammoTimer+= deltaTime
+    }
+    this.enemies.forEach(enemy => {
+      enemy.update()
+      if (this.checkCollision(this.player, enemy)) {
+          enemy.markedForDeletion = true
+      }
+      this.player.projectiles.forEach(projectile => {
+        if (this.checkCollision(projectile, enemy)) {
+            //marking for deletion will make removing easier
+            projectile.markedForDeletion = true
+            enemy.lives--
             
-              if (enemy.lives <= 0) {
-                enemy.markedForDeletion = true
-                if(!this.gameOver)this.score+= enemy.score
-                if (this.score >= this.winningScore) this.gameOver = true
-              }
-          }
-        })       
-      })  
+          
+            if (enemy.lives <= 0) {
+              enemy.markedForDeletion = true
+              if(!this.gameOver)this.score+= enemy.score
+              if (this.score >= this.winningScore) this.gameOver = true
+            }
+        }
+      })       
+    })  
 
-    
-      if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
-          this.addEnemy()
-          this.enemyTimer = 0
-      } else {
-         this.enemyTimer += deltaTime
-      }
-      this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion) //removes ALL marked-for-delete enemies if they pass the collision check
+  
+    if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+        this.addEnemy()
+        this.enemyTimer = 0
+    } else {
+        this.enemyTimer += deltaTime
     }
-    draw(context) {
-      this.backGround.draw(context) //background must be drawn first so it is in the BACK of the player
-      this.player.draw(context) //note game.draw() will just call player.draw() passing in curent canvas we wanan draw on
-      this.backGround.drawLastLayer(context)
-      this.ui.draw(context)
-      this.enemies.forEach(enemy => {
-        enemy.draw(context)
-      })
-    }
-    addEnemy() {
-      this.enemies.push(new Angler1(this)) //passing 'this' aka game obj since Angler class expects a game argument
-    }  
-    checkCollision(rect1,rect2) {
-      return ( //basic rectangle collision formula 
-        rect1.x < rect2.x + rect2.width &&
-        rect2.x < rect1.x + rect1.width &&
-        rect1.y < rect2.y + rect2.height &&
-        rect2.y < rect1.y + rect1.height
-      )
-    }
+    this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion) //removes ALL marked-for-delete enemies if they pass the collision check
   }
+  draw(context) {
+    this.backGround.draw(context) //background must be drawn first so it is in the BACK of the player
+    this.player.draw(context) //note game.draw() will just call player.draw() passing in curent canvas we wanan draw on
+    this.backGround.drawLastLayer(context)
+    this.ui.draw(context)
+    this.enemies.forEach(enemy => {
+      enemy.draw(context)
+    })
+  }
+  addEnemy() {
+    const randomize = Math.random() 
+
+    this.enemies.push(randomize < 0.3 ?
+       new Angler1(this) : randomize < 0.6 ?
+       new Angler2(this) : new LuckyFish(this)) 
+  }  
+  checkCollision(rect1,rect2) {
+    return ( //basic rectangle collision formula 
+      rect1.x < rect2.x + rect2.width &&
+      rect2.x < rect1.x + rect1.width &&
+      rect1.y < rect2.y + rect2.height &&
+      rect2.y < rect1.y + rect1.height
+    )
+  }
+}
   // the code below will instantiate a new game AND player on document load.
   const game = new Game(canvas.width, canvas.height)
   let lastTime = 0
-  function animate(timeStamp) {
-    const deltaTime = timeStamp - lastTime  
 
+  let msPrev = window.performance.now()
+  let fps = 60
+  let msPerFrame = 1000 / fps
+
+
+  function animate(timeStamp) {
+
+    // if (game.player.y + game.player.height >= canvas.height) return
+    window.requestAnimationFrame(animate)
+
+    const deltaTime = timeStamp - lastTime  
     lastTime = timeStamp
+// this code block below will force 60FPS (for slower computers)
+
+    // const msNow = window.performance.now()
+    // const msPassed = msNow - msPrev       
+
+
+  
+    // if (msPassed < msPerFrame) return
+  
+    // const excessTime = msPassed % msPerFrame
+    // msPrev = msNow - excessTime
+ //---------------end of 60FPS enforcement------------------
+
     ctx.clearRect(0,0,canvas.width,canvas.height)
     game.update(deltaTime) //note this ALSO calls game.player.draw(), spawns projectiles on space-bar press, checks for collision between players/enemies and ALSO projectiles against all current enemies
     game.draw(ctx)
-    // if (game.player.y + game.player.height >= canvas.height) return
-    requestAnimationFrame(animate)
+
   }
- 
   animate(0)
 
 
